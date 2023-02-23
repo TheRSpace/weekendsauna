33ca592f7745d8788612e920118c84c7de9b4e60
Raimonds <raimmonds@gmail.com>
2023-02-24 01:33:52 +0200

    major update navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import './App.css';
+
 
 const App = () => {
     return (
-        <div>
-            <h1>Cau mammu</h1>
-        </div>
+        <>
+      <Router>
+        <Navbar />
+        {/*<Switch>
+          <Route path='/' exact component={Home} />
+          <Route path='/services' component={Services} />
+          <Route path='/products' component={Products} />
+    <Route path='/sign-up' component={SignUp} />
+        </Switch>*/}
+      </Router>
+    </>
+
     );
 };
 
