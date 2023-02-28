300739f75564ce90c9b2c193838e513b2759bd51
Raimonds <raimmonds@gmail.com>
2023-02-28 03:28:08 +0200

    fixed home

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,15 +1,14 @@
 import React from "react";
 import "../../App.css";
-import Cards from "../Cards";
+//import Cards from "../Cards";
 import HeroSection from "../HeroSection";
 import Footer from "../Footer";
-/*import { MDBCard, MDBCardBody, MDBCardTitle, MDBCardText, MDBCardImage, MDBBtn } from "mdb-react-ui-kit";
- */
+//import { MDBCard, MDBCardBody, MDBCardTitle, MDBCardText, MDBCardImage, MDBBtn } from "mdb-react-ui-kit";
+
 function Home() {
   return (
     <>
       <HeroSection />
-      <Cards />
       <Footer />
     </>
   );
