c0ad896bc84dd56fc10352c32262dbfde79045f1
Raimonds <raimmonds@gmail.com>
2023-02-28 17:48:38 +0200

    description

--- src/App.js
+++ src/App.js
@@ -8,10 +8,19 @@ import Home from "./components/pages/Home";
 import Products from "./components/pages/Products";
 import SignUp from "./components/pages/SignUp";
 import About from "./components/pages/About";
+import { Helmet } from "react-helmet";
 
 const App = () => {
   return (
     <>
+      <div className="application">
+        <Helmet>
+          <meta charSet="utf-8" />
+          <title>Weekend Sauna</title>
+          <link rel="canonical" href="https://www.weekendsauna.com/" />
+          <meta name="description" content="Weekend Sauna pirts noma latvijā pasākumiem, ballītei vai atpūtai." />
+        </Helmet>
+      </div>
       <Router>
         <Navbar />
         <Routes>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -30,7 +30,7 @@ function Navbar() {
         <div className="navbar-container">
           <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
             Weekend Sauna
-            <i class="fa-brands fa-instagram" />
+            {/*<i class="fa-brands fa-instagram" />*/}
           </Link>
           <div className="menu-icon" onClick={handleClick}>
             <i className={click ? "fas fa-times" : "fas fa-bars"} />
@@ -60,7 +60,7 @@ function Navbar() {
               <Link to="/services" className="nav-links" onClick={closeMobileMenu}>
                 REĢISTRĒTIES
               </Link>
-  </li>*/}
+              </li>*/}
           </ul>
           {button && <Button buttonStyle="btn--outline">Sign Up</Button>}
         </div>
